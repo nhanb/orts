2e3326c4c6b36501210c78543039c656563a9d71
nhanb <nhan@nerdyweekly.com>
2018-10-26 00:58:18 +0700

    working state-writing

--- orts.py
+++ orts.py
@@ -1,17 +1,11 @@
 import json
-from tkinter import Tk, N, S, E, W, Spinbox
+from tkinter import Tk, N, S, E, W, Spinbox, StringVar
 from tkinter import ttk
 
 
-def apply_state(state={"hello": "world"}):
-    with open("./html/state.json", "w") as state_file:
-        content = json.dumps(state, indent=2)
-        state_file.write(content)
+def build_ui(root, state, apply_state):
 
-
-def build_ui():
     # 2 main frames:
-    root = Tk()
     content = ttk.Frame(root, padding=(3, 3, 12, 12))
     actions = ttk.Frame(root, padding=(3, 3, 12, 12))
     content.grid(column=0, row=0, sticky=(N, S, E, W))
@@ -20,10 +14,10 @@ def build_ui():
     # Content:
     name1lbl = ttk.Label(content, text="Player 1")
     name2lbl = ttk.Label(content, text="Player 2")
-    name1 = ttk.Entry(content)
-    name2 = ttk.Entry(content)
-    score1 = Spinbox(content, from_=0, to=7777)
-    score2 = Spinbox(content, from_=0, to=7777)
+    name1 = ttk.Entry(content, textvariable=state["p1name"])
+    name2 = ttk.Entry(content, textvariable=state["p2name"])
+    score1 = Spinbox(content, from_=0, to=7777, textvariable=state["p1score"])
+    score2 = Spinbox(content, from_=0, to=7777, textvariable=state["p2score"])
     name1lbl.grid(column=0, row=0)
     name1.grid(column=1, row=0)
     score1.grid(column=2, row=0)
@@ -41,4 +35,19 @@ def build_ui():
 
 
 if __name__ == "__main__":
-    build_ui()
+    root = Tk()
+
+    state = {
+        "p1name": StringVar(),
+        "p1score": StringVar(),
+        "p2name": StringVar(),
+        "p2score": StringVar(),
+    }
+
+    def apply_state():
+        with open("./html/state.json", "w") as state_file:
+            state_json = {field: var.get() for field, var in state.items()}
+            content = json.dumps(state_json, indent=2)
+            state_file.write(content)
+
+    build_ui(root, state, apply_state)
