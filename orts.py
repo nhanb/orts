c433e682c52e602cde4178ee2415c878e1689987
nhanb <nhan@nerdyweekly.com>
2018-10-26 00:58:51 +0700

    swap both scores & names

--- orts.py
+++ orts.py
@@ -4,7 +4,7 @@ from tkinter import Tk, N, S, E, W, Spinbox, StringVar
 from tkinter import ttk
 
 
-def build_ui(root, state, apply_state, reset_scores, swap_scores):
+def build_ui(root, state, apply_state, reset_scores, swap_players):
 
     # 2 main frames:
     content = ttk.Frame(root, padding=(3, 3, 12, 12))
@@ -29,7 +29,7 @@ def build_ui(root, state, apply_state, reset_scores, swap_scores):
     # Actions:
     apply = ttk.Button(actions, text="Apply", command=apply_state)
     reset = ttk.Button(actions, text="Reset", command=reset_scores)
-    swap = ttk.Button(actions, text="Swap", command=swap_scores)
+    swap = ttk.Button(actions, text="Swap", command=swap_players)
     apply.grid(column=0, row=0, padx=5)
     reset.grid(column=1, row=0, padx=5)
     swap.grid(column=2, row=0, padx=5)
@@ -38,7 +38,7 @@ def build_ui(root, state, apply_state, reset_scores, swap_scores):
 
 
 def apply_state(state):
-    with open("./html/state.json", "w") as state_file:
+    with open("./web/state.json", "w") as state_file:
         state_json = {field: var.get() for field, var in state.items()}
         content = json.dumps(state_json, indent=2)
         state_file.write(content)
@@ -49,10 +49,13 @@ def reset_scores(state):
     state["p2score"].set("0")
 
 
-def swap_scores(state):
+def swap_players(state):
     score1, score2 = state["p1score"].get(), state["p2score"].get()
+    name1, name2 = state["p1name"].get(), state["p2name"].get()
     state["p1score"].set(score2)
     state["p2score"].set(score1)
+    state["p1name"].set(name2)
+    state["p2name"].set(name1)
 
 
 if __name__ == "__main__":
@@ -71,5 +74,5 @@ if __name__ == "__main__":
         state,
         apply_state=partial(apply_state, state),
         reset_scores=partial(reset_scores, state),
-        swap_scores=partial(swap_scores, state),
+        swap_players=partial(swap_players, state),
     )
