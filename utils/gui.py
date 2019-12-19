5c9d7c5d92e1a270b5f3bdf69db96881fc966efa
Bùi Thành Nhân <hi@imnhan.com>
2019-12-19 21:29:27 +0700

    working persistent state with apply/discard

--- utils/gui.py
+++ utils/gui.py
@@ -7,3 +7,10 @@ def load_icon(filename):
         data = f.read()
         image = enaml.image.Image(data=data)
         return enaml.icon.Icon(images=[enaml.icon.IconImage(image=image)])
+
+
+def swap_players(state):
+    state.p1name, state.p2name = state.p2name, state.p1name
+    state.p1country, state.p2country = state.p2country, state.p1country
+    state.p1score, state.p2score = state.p2score, state.p1score
+    state.p1team, state.p2team = state.p2team, state.p1team
